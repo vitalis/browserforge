d1fa4f3f260db37b68cda3a9d1fb8008d00044eb
Vitaly Gorodetsky <gorodetsky@gmail.com>
2024-12-19 11:10:21 +0200

    chore: update mix.exs for hex.pm publush

--- mix.exs
+++ mix.exs
@@ -8,7 +8,20 @@ defmodule BrowserForge.MixProject do
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+
+      # Hex.pm specific fields
+      description: "Intelligent browser header & fingerprint generator for Elixir",
+      package: package(),
+
+      # Docs
+      name: "BrowserForge",
+      source_url: "https://github.com/vitalis/browserforge",
+      homepage_url: "https://github.com/vitalis/browserforge",
+      docs: [
+        main: "BrowserForge",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,10 +35,27 @@ defmodule BrowserForge.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"},
-      {:finch, "~> 0.19"}
+      {:finch, "~> 0.19"},
+
+      # Dev and test dependencies
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      name: "browserforge",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/vitalis/browserforge"
+      },
+      maintainers: ["Vitaly Gorodetsky"]
+    ]
+  end
 end
